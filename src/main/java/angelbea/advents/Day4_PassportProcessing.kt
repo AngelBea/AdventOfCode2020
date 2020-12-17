f70e6db6639459a8210a7409bc127a55c68a4564
AngelBea <angel.bea.gonzalez@gmail.com>
2020-12-17 18:54:35 +0100

    ABG --> Day 4 Completed

--- src/main/java/angelbea/advents/Day4_PassportProcessing.kt
+++ src/main/java/angelbea/advents/Day4_PassportProcessing.kt
@@ -15,6 +15,7 @@ class Day4_PassportProcessing : IAdventCodes{
 
         input.forEachIndexed { index, passportLine ->
             val passport = Passport(
+                index,
                 Extractors.BIRTH_YEAR.extractor.find(passportLine)?.value?.replace("\\w*:".toRegex(), ""),
                 Extractors.ISSUE_YEAR.extractor.find(passportLine)?.value?.replace("\\w*:".toRegex(), ""),
                 Extractors.EXPIRATION_YEAR.extractor.find(passportLine)?.value?.replace("\\w*:".toRegex(), ""),
@@ -35,21 +36,50 @@ class Day4_PassportProcessing : IAdventCodes{
     }
 
 
-    data class Passport(var birthYear : String?, var issueYear : String?, var expirationYear : String?, var height : String?,
+    data class Passport(val passportIndex : Int, var birthYear : String?, var issueYear : String?, var expirationYear : String?, var height : String?,
                         var hairColor : String?, var eyeColor : String?, var passportId : String?, var countryId : String?)
 
     fun Passport.isValid(): Boolean{
-        return listOfNotNull(birthYear, issueYear, expirationYear, height, hairColor, eyeColor, passportId).size >= 7
+        if (this.birthYear == null) {
+            println("$passportIndex NOT VALID --> Reason: Birth Year is null")
+            return false
+        }
+        if (this.issueYear == null) {
+            println("$passportIndex NOT VALID --> Reason: Issue Year is null")
+            return false
+        }
+        if (this.expirationYear == null) {
+            println("$passportIndex NOT VALID --> Reason: Expiration Year is null")
+            return false
+        }
+        if (this.height == null) {
+            println("$passportIndex NOT VALID --> Reason: Height is null")
+            return false
+        }
+        if (this.hairColor == null) {
+            println("$passportIndex NOT VALID --> Reason: Hair Color is null")
+            return false
+        }
+        if (this.eyeColor == null) {
+            println("$passportIndex NOT VALID --> Reason: Eye Color is null")
+            return false
+        }
+        if (this.passportId == null) {
+            println("$passportIndex NOT VALID --> Reason: Passport Id is null")
+            return false
+        }
+
+        return true
     }
 
     enum class Extractors(val extractor : Regex){
         BIRTH_YEAR("byr:(19[2-9][0-9]|200[0-2])(?![\\d\\w])".toRegex()),
         ISSUE_YEAR("iyr:(201[0-9]|2020)(?![\\d\\w])".toRegex()),
         EXPIRATION_YEAR("eyr:(202[0-9]|2030)(?![\\d\\w])".toRegex()),
-        HEIGHT("hgt:(15[0-9]|16[0-9]|17[0-9]|18[0-9]|19[0-3])cm|hgt:(59|6[0-9]|7[0-6])in".toRegex()),
-        HAIR_COLOR("hcl:#[a-f0-9]{6}".toRegex()),
-        EYE_COLOR("ecl:((amb)|(blu)|(brn)|(gry)|(grn)|(hzl)|(oth))(?!\\w)*".toRegex()),
-        PASSPORT_ID("pid:\\d{9}".toRegex()),
+        HEIGHT("hgt:(15[0-9]|16[0-9]|17[0-9]|18[0-9]|19[0-3])cm(?![\\d\\w])|hgt:(59|6[0-9]|7[0-6])in(?![\\d\\w])".toRegex()),
+        HAIR_COLOR("hcl:#[a-f0-9]{6}(?![\\d\\w])".toRegex()),
+        EYE_COLOR("ecl:((amb)|(blu)|(brn)|(gry)|(grn)|(hzl)|(oth))(?![\\d\\w])*".toRegex()),
+        PASSPORT_ID("pid:\\d{9}(?![\\d\\w])".toRegex()),
         COUNTRY_ID("cid:\\w*".toRegex())
     }
 }
